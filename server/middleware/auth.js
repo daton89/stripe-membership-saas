0a8f7479cba209a9c9d37d48479500044928632f
Toni D'Angelo <tonydangelo123@gmail.com>
2018-05-09 16:09:32 +0200

    update auth with token and main app

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,47 +1,58 @@
-'use strict';
-const rp = require('request-promise');
-const secrets = require('../config/secrets');
+"use strict";
+const rp = require("request-promise");
+const secrets = require("../config/secrets");
 
-module.exports.isAuthenticated = function () {
-    return async function (req, res, next) {
+module.exports.isAuthenticated = function() {
+  return async function(req, res, next) {
+    if (!req.user || !req.user.token) return res.redirect(req.redirect.auth);
 
-        if (!req.user || !req.user.token) return res.redirect(req.redirect.auth);
+    try {
+      const user = await rp({
+        method: "GET",
+        uri: secrets.auth.host + "api/v1/auth/whoami",
+        headers: {
+          authorization: "Bearer " + req.user.token
+        },
+        json: true
+      });
 
-        try {
-
-            const isValid = await rp({
-                method: 'GET',
-                uri: secrets.auth.host + 'verify/' + req.user.token
-            })
-
-            if (isValid) return next();
-
-        } catch (err) {
-            res.redirect(req.redirect.auth);
-        }
-
+      if (user.token) {
+        req.session.passport.user.token = user.token;
+        // console.log(
+        //   "req.session.passport.user.token =>",
+        //   req.session.passport.user.token
+        // );
+        return next();
+      } else {
+        res.redirect(req.redirect.auth);
+      }
+    } catch (err) {
+      res.redirect(req.redirect.auth);
     }
+  };
 };
 
-module.exports.isUnauthenticated = function () {
-    return async function (req, res, next) {
+module.exports.isUnauthenticated = function() {
+  return async function(req, res, next) {
+    if (!req.user || !req.user.token) return next();
 
-        if (!req.user || !req.user.token) return next();
+    try {
+      const user = await rp({
+        method: "GET",
+        uri: secrets.auth.host + "api/v1/auth/whoami",
+        headers: {
+          authorization: "Bearer " + req.user.token
+        },
+        json: true
+      });
 
-        try {
-
-            const isValid = await rp({
-                method: 'GET',
-                uri: secrets.auth.host + 'verify/' + req.user.token
-            })
-
-            if (!isValid) return next();
-
-        } catch (err) {
-            return next();
-        }
-
-        res.redirect(req.redirect.auth);
-
+      if (!user.token) {
+        return next();
+      }
+    } catch (err) {
+      return next();
     }
+
+    res.redirect(req.redirect.auth);
+  };
 };
--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -1,18 +1,18 @@
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
-var rp = require('request-promise');
-const secrets = require('../config/secrets');
-const moment = require('moment')
+var LocalStrategy = require("passport-local").Strategy;
+var User = require("../models/user");
+var rp = require("request-promise");
+const secrets = require("../config/secrets");
+const moment = require("moment");
 
-module.exports = function (passport) {
-
-  passport.serializeUser(function (user, done) {
-    done(null, user.email);
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user);
   });
 
-  passport.deserializeUser(async function (email, done) {
+  passport.deserializeUser(async function(user, done) {
     try {
-      const user = await User.findOne({ email });
+      // console.log("deserializeUser =>", user);
+      // const user = await User.findOne({ email });
       done(null, user);
     } catch (err) {
       done(err);
@@ -20,107 +20,139 @@ module.exports = function (passport) {
   });
 
   // login
-  passport.use('login', new LocalStrategy({
-    usernameField: 'email',
-    passReqToCallback: true
-  },
-    async function (req, email, password, done) {
-
-      try {
-
-        const signin = await rp({
-          method: 'POST',
-          uri: secrets.auth.host + 'access/signin',
-          body: {
-            email,
-            password
-          },
-          json: true
-        });
-
-        if (signin.status === 200) {
-          const token = signin.message.token;
-          // const expires_in = signup.message.expires_in;
-          const expires_in = moment().add(10, 'minutes');
-          const user = await User.findOneAndUpdate({ email }, { token, expires_in }, { new: true });
-          return done(null, user, req.flash('success', 'Successfully logged in.'));
-        }
-
-      } catch (err) {
-        return done(err, false, req.flash('error', err.message || 'Unrecognized error'));
-      }
-
-    })
-  );
-
-  passport.use('signup', new LocalStrategy({
-    usernameField: 'email',
-    passReqToCallback: true
-  },
-    function (req, email, password, done) {
-      var findOrCreateUser = async function () {
-
+  passport.use(
+    "login",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passReqToCallback: true
+      },
+      async function(req, email, password, done) {
         try {
-
+          // TODO: call /api/v1/auth/login on auth host
           const signin = await rp({
-            method: 'POST',
-            uri: secrets.auth.host + 'access/signin',
+            method: "POST",
+            uri: secrets.auth.host + "api/v1/auth/login",
             body: {
-              email,
+              username: email,
               password
             },
             json: true
           });
+          console.log("signin =>", signin);
 
-          req.flash('form', {
-            email: req.body.email
+          // const token = signin.token;
+          // const expires_in = signup.message.expires_in;
+          // const expires_in = moment().add(15, 'minutes');
+          // const user = await User.findOneAndUpdate({ email }, { token, expires_in }, { new: true });
+          req.login(signin, err => {
+            if (err)
+              return done(
+                err,
+                false,
+                req.flash("error", err.message || "Unrecognized error")
+              );
+
+            return done(null, signin, null);
           });
-
-          return done(null, false, req.flash('error', 'An account with that email address already exists.'));
-
         } catch (err) {
-
-          if (err.statusCode === 404) {
-            let signup;
-            try {
-              signup = await rp({
-                method: 'POST',
-                uri: secrets.auth.host + 'access/signup',
-                body: {
-                  name: email, // remove
-                  phone: email, // remove
-                  email,
-                  password
-                },
-                json: true
-              });
-            } catch (err) {
-              return done(err, false, req.flash('error', 'Error singup user.'));
-            }
-            console.log('signup =>', signup);
-            try {
-              // edit this portion to accept other properties when creating a user.
-              const token = signup.message.token;
-              // const expires_in = signup.message.expires_in;
-              const expires_in = moment().add(10, 'minutes');
-              const user = await User.create({
-                email: req.body.email,
-                token,
-                expires_in
-              });
-              return done(null, user, req.flash('success', 'Thanks for signing up!!'));
-            } catch (err) {
-              return done(err, false, req.flash('error', 'Error saving user.'));
-            }
-
-          } else {
-            return done(err, false, req.flash('error', 'Error signup user.'));
-          }
+          return done(
+            err,
+            false,
+            req.flash("error", err.message || "Unrecognized error")
+          );
         }
-      };
+      }
+    )
+  );
 
-      process.nextTick(findOrCreateUser);
+  passport.use(
+    "signup",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passReqToCallback: true
+      },
+      function(req, email, password, done) {
+        var findOrCreateUser = async function() {
+          return done(true, false, req.flash("error", "Error singup user."));
 
-    })
+          try {
+            const signin = await rp({
+              method: "POST",
+              uri: secrets.auth.host + "access/signin",
+              body: {
+                email,
+                password
+              },
+              json: true
+            });
+
+            req.flash("form", {
+              email: req.body.email
+            });
+
+            return done(
+              null,
+              false,
+              req.flash(
+                "error",
+                "An account with that email address already exists."
+              )
+            );
+          } catch (err) {
+            if (err.statusCode === 404) {
+              let signup;
+              try {
+                signup = await rp({
+                  method: "POST",
+                  uri: secrets.auth.host + "access/signup",
+                  body: {
+                    name: email, // remove
+                    phone: email, // remove
+                    email,
+                    password
+                  },
+                  json: true
+                });
+              } catch (err) {
+                return done(
+                  err,
+                  false,
+                  req.flash("error", "Error singup user.")
+                );
+              }
+              console.log("signup =>", signup);
+              try {
+                // edit this portion to accept other properties when creating a user.
+                const token = signup.message.token;
+                // const expires_in = signup.message.expires_in;
+                const expires_in = moment().add(10, "minutes");
+                const user = await User.create({
+                  email: req.body.email,
+                  token,
+                  expires_in
+                });
+                return done(
+                  null,
+                  user,
+                  req.flash("success", "Thanks for signing up!!")
+                );
+              } catch (err) {
+                return done(
+                  err,
+                  false,
+                  req.flash("error", "Error saving user.")
+                );
+              }
+            } else {
+              return done(err, false, req.flash("error", "Error signup user."));
+            }
+          }
+        };
+
+        process.nextTick(findOrCreateUser);
+      }
+    )
   );
 };
