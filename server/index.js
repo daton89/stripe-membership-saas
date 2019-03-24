1c228860f3b1af8890225ab0f8ffdebf881c3221
Toni D'Angelo <tonydangelo123@gmail.com>
2019-03-24 22:52:03 +0100

    throw dotenv err

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
-require('dotenv').config();
-var debug = require('debug')('app');
-var app = require('./app');
+import dotenv from "dotenv";
+
+const loaded = dotenv.config();
+
+if (loaded.error) throw loaded.error;
+
+console.log("loaded.parsed :", loaded.parsed);
+
+var debug = require("debug")("app");
+var app = require("./app");
 // TODO: config with port
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + server.address().port);
+var server = app.listen(app.get("port"), function() {
+  console.log("Express server listening on port " + server.address().port);
 });
